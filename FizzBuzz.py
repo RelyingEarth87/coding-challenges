d92b063e7a1c8f0a250a8285438ee882bcadd3d7
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2024-06-09 04:31:30 -0400

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+def fizzbuzz(n: int):
+    output = []
+    for i in range(1, n + 1):
+        current = ''
+        if i % 3 == 0:
+            current += 'Fizz'
+        if i % 5 == 0:
+            current += 'Buzz'
+        if i % 5 != 0 and i % 3 != 0:
+            current += str(i)
+        output.append(current)
+    
+    return output
+
+def main():
+    print(fizzbuzz(3))
+    print(fizzbuzz(5))
+    print(fizzbuzz(15))
+
+
+if __name__ == '__main__':
+    main()
