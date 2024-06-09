40d2f2d9ea742e321dae14c6aa8eee4c3db55f7b
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2024-06-09 04:33:23 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 def fizzbuzz(n: int):
-    output = []
+    output: list = []
     for i in range(1, n + 1):
-        current = ''
+        current: str = ''
         if i % 3 == 0:
             current += 'Fizz'
         if i % 5 == 0:
