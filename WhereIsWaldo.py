f4a741d14cf22292be00705cccf04d2f4709b10e
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2024-08-15 20:18:13 -0400

    Add files via upload

--- WhereIsWaldo.py
+++ WhereIsWaldo.py
@@ -1,4 +1,12 @@
 def whereIsWaldo(arr: list[str]) -> list[int]:
+    """Finds the position of the different string in a given array
+
+    Args:
+        arr (list[str]): a box or rectangle-shaped array where all elements are the same except one
+
+    Returns:
+        list[int]: A position, consisting of a row and a column number, where the item is different than every other item
+    """
     import statistics
     for i in range(len(arr)):
         if len(set(arr[i])) == 2:
@@ -15,6 +23,8 @@ def whereIsWaldo(arr: list[str]) -> list[int]:
 
 
 def main() -> None:
+    """A main entry point to handle test cases and pretty printing
+    """
     test_cases = [[["A", "A", "A"], ["A", "A", "A"], ["A", "B", "A"]], [["c", "c", "c", "c"], ["c", "c", "c", "d"]], [["O", "O", "O", "O"], ["O", "O", "O", "O"], ["O", "O", "O", "O"], ["O", "O", "O", "O"], ["P", "O", "O", "O"], ["O", "O", "O", "O"]]]
     answers = [[3, 2], [2, 4], [5, 1]]
 
